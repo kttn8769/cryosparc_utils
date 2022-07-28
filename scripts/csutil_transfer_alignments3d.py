4554d55c94f0a000509952f0d64ee21d9966175e
kttn8769 <kttn8769@gmail.com>
2022-07-28 21:26:27 +0900

    Speed up

--- scripts/csutil_transfer_alignments3d.py
+++ scripts/csutil_transfer_alignments3d.py
@@ -4,21 +4,20 @@
 import sys
 import os
 import argparse
+import time
 import pandas as pd
 import numpy as np
 from cryosparc_compute import dataset
 
 
-def get_blobpath_basename(arr, dont_remove_uuid=True):
+def get_blobpath_basename(arr, num_remove_uuid):
     out_arr = []
     for s in arr:
         # Filename
         s = os.path.basename(s)
         # Remove file extension
         s = os.path.splitext(s)[0]
-        if not dont_remove_uuid:
-            # Split by '_', discard the first word (new UID), then concatenate
-            s = '_'.join(s.split('_')[1:])
+        s = '_'.join(s.split('_')[num_remove_uuid:])
         out_arr.append(s)
     if isinstance(arr, np.ndarray):
         out_arr = np.array(out_arr)
@@ -33,8 +32,9 @@ def parse_args():
     parser.add_argument('--orig_cs_file', required=True, help='The extracted_particles.cs file of the extraction job with binning.')
     parser.add_argument('--orig_passthrough_file', required=True, help='The P*_J*_passthrough_particles.cs file of the extraction job with binning.')
     parser.add_argument('--imported_cs_file', required=True, help='The imported_particles.cs file of the import job.')
-    parser.add_argument('--output_cs_file', required=True, help='Output cs file.')
-    parser.add_argument('--dont_remove_blobpath_uuid', action='store_true', help='Do not remove UUIDs from the imported blobpaths')
+    parser.add_argument('--output_cs_file', required=True, help='Output cs file.'),
+    parser.add_argument('--orig_num_remove_blobpath_uuid', type=int, default=1, help='Preceding UUID strings will be removed from blobpaths of the original dataset this many times.')
+    parser.add_argument('--imported_num_remove_blobpath_uuid', type=int, default=2, help='Preceding UUID strings will be removed from blobpaths of the imported dataset this many times.')
 
     args = parser.parse_args()
 
@@ -64,40 +64,127 @@ def main():
     print(f'Combining the original dataset and passthrough infos...')
     orig_dataset_passthrough = dataset.Dataset().from_dataset(orig_passthrough).innerjoin(orig_dataset)
 
-    # np.ndarray containing the particle image paths
-    imported_blobpaths = imported_dataset.data['blob/path']
-    imported_blobpaths_basename = get_blobpath_basename(imported_blobpaths, dont_remove_uuid=args.dont_remove_blobpath_uuid)
-    imported_blobpaths_basename_uniq = np.unique(imported_blobpaths_basename)
-
+    print(f'Preparing the original dataset infos')
     df_orig = orig_dataset_passthrough.to_dataframe()
+    # Convert to numpy array for speeding up.
+    arr_orig = df_orig.to_numpy(copy=True)
+    cols_orig = list(df_orig.columns)
+    cols_orig_colidx_blobpath = cols_orig.index('blob/path')
+    cols_orig_colidx_blobidx = cols_orig.index('blob/idx')
+    print(f'Original blobpath example: {arr_orig[0, cols_orig_colidx_blobpath]}')
+    orig_blobpaths_basename = get_blobpath_basename(
+        arr_orig[:, cols_orig_colidx_blobpath].flatten().astype(str),
+        args.orig_num_remove_blobpath_uuid
+    )
+    print(f'Original blobpath basename example: {orig_blobpaths_basename[0]}')
+    # Sort by blobpath basename for faster search
+    arr_orig_sort_idxs = np.argsort(orig_blobpaths_basename, axis=0)
+    arr_orig = arr_orig[arr_orig_sort_idxs]
+    orig_blobpaths_basename_sorted = orig_blobpaths_basename[arr_orig_sort_idxs]
+
+    print(f'Preparing the imported dataset infos')
     df_imported = imported_dataset.to_dataframe()
-    df_output = pd.DataFrame()
+    arr_imported = df_imported.to_numpy(copy=True)
+    cols_imported = list(df_imported.columns)
+    cols_imported_colidx_blobpath = cols_imported.index('blob/path')
+    cols_imported_colidx_blobidx = cols_imported.index('blob/idx')
+    print(f'Imported blobpath example: {arr_imported[0, cols_imported_colidx_blobpath]}')
+    imported_blobpaths_basename = get_blobpath_basename(
+        arr_imported[:, cols_imported_colidx_blobpath].flatten().astype(str),
+        args.imported_num_remove_blobpath_uuid
+    )
+    print(f'Imported blobpath basename example: {imported_blobpaths_basename[0]}')
+    arr_imported_sort_idxs = np.argsort(imported_blobpaths_basename, axis=0)
+    arr_imported = arr_imported[arr_imported_sort_idxs]
+    imported_blobpaths_basename_sorted = imported_blobpaths_basename[arr_imported_sort_idxs]
+    imported_blobpaths_basename_uniq = np.unique(imported_blobpaths_basename_sorted)
 
-    alignments3d_cols = [col for col in df_imported.columns if 'alignments3D/' in col]
+    alignments3d_cols = [col for col in cols_imported if 'alignments3D/' in col]
 
+    output_df_list = []
+
+    print('Processing ....')
+    n = len(imported_blobpaths_basename_uniq)
+    progbar_length = 100
+    progbar_step = int(n / progbar_length)
+    # progbar_step = 1
+    num_step = 0
     for i in range(len(imported_blobpaths_basename_uniq)):
-        if (i != 0) and (i % 10 == 0):
-            print(f'Processed {i} particle stacks.')
+    # for i in range(10):
+        if i % progbar_step == 0:
+            sys.stdout.write('\r')
+            sys.stdout.write(
+                'Progress: [{:{}}] ({:7d} / {:7d})'
+                .format(
+                    '|' * num_step,
+                    progbar_length,
+                    i + 1,
+                    n
+                )
+            )
+            num_step += 1
 
         blobpath_query = imported_blobpaths_basename_uniq[i]
 
-        df_imported_queried = df_imported.loc[df_imported['blob/path'].str.contains(blobpath_query)]
-        df_imported_queried_sort = df_imported_queried.sort_values('blob/idx', axis=0, inplace=False, ignore_index=True)
+        arr_imported_same_blobpath_end_idx = len(imported_blobpaths_basename_sorted)
+        for j in range(len(imported_blobpaths_basename_sorted)):
+            if blobpath_query != imported_blobpaths_basename_sorted[j]:
+                arr_imported_same_blobpath_end_idx = j
+                break
+        arr_imported_same_blobpath = arr_imported[:arr_imported_same_blobpath_end_idx]
+        arr_imported_same_blobpath = arr_imported_same_blobpath[
+            np.argsort(arr_imported_same_blobpath[:, cols_imported_colidx_blobidx].flatten().astype(np.int))
+        ]
+        arr_imported = arr_imported[arr_imported_same_blobpath_end_idx:]
+        imported_blobpaths_basename_sorted = imported_blobpaths_basename_sorted[arr_imported_same_blobpath_end_idx:]
 
-        df_orig_queried = df_orig.loc[df_orig['blob/path'].str.contains(blobpath_query)]
-        df_orig_idxmatched = df_orig_queried.loc[df_orig_queried['blob/idx'].isin(df_imported_queried_sort['blob/idx'])]
-        df_orig_idxmatched_sort = df_orig_idxmatched.sort_values('blob/idx', axis=0, inplace=False, ignore_index=True)
+        for j in range(len(orig_blobpaths_basename_sorted)):
+            if blobpath_query == orig_blobpaths_basename_sorted[j]:
+                break
+        arr_orig_same_blobpath_start_idx = j
+        arr_orig_same_blobpath_end_idx = len(orig_blobpaths_basename_sorted)
+        for j in range(arr_orig_same_blobpath_start_idx, len(orig_blobpaths_basename_sorted)):
+            if blobpath_query != orig_blobpaths_basename_sorted[j]:
+                arr_orig_same_blobpath_end_idx = j
+                break
+        arr_orig_same_blobpath = arr_orig[arr_orig_same_blobpath_start_idx:arr_orig_same_blobpath_end_idx]
+        arr_orig_same_blobpath = arr_orig_same_blobpath[
+            np.argsort(arr_orig_same_blobpath[:, cols_orig_colidx_blobidx].flatten().astype(np.int))
+        ]
+        arr_orig = arr_orig[arr_orig_same_blobpath_end_idx:]
+        orig_blobpaths_basename_sorted = orig_blobpaths_basename_sorted[arr_orig_same_blobpath_end_idx:]
 
-        assert np.all(df_orig_idxmatched_sort['blob/idx'].values == df_imported_queried_sort['blob/idx'].values)
+        arr_orig_same_blobpath_match_or_not = np.isin(
+            arr_orig_same_blobpath[:, cols_orig_colidx_blobidx].astype(int),
+            arr_imported_same_blobpath[:, cols_imported_colidx_blobidx].astype(int),
+            assume_unique=True
+        )
+        arr_orig_same_blobpath_matched = arr_orig_same_blobpath[arr_orig_same_blobpath_match_or_not]
+        # Sort by blob/idx
+        arr_orig_same_blobpath_matched = arr_orig_same_blobpath_matched[
+            np.argsort(arr_orig_same_blobpath_matched[:, cols_orig_colidx_blobidx].flatten().astype(np.int), axis=0)
+        ]
+
+        assert np.all(arr_imported_same_blobpath[:, cols_imported_colidx_blobidx] == arr_orig_same_blobpath_matched[:, cols_orig_colidx_blobidx])
+
+
+        df_imported_same_blobpath = pd.DataFrame(arr_imported_same_blobpath, columns=cols_imported)
+        df_orig_same_blobpath_matched = pd.DataFrame(arr_orig_same_blobpath_matched, columns=cols_orig)
 
         # Transfer the 3D poses
-        df_orig_idxmatched_sort[alignments3d_cols] = df_imported_queried_sort[alignments3d_cols]
+        df_orig_same_blobpath_matched[alignments3d_cols] = df_imported_same_blobpath[alignments3d_cols]
 
-        df_output = df_output.append(df_orig_idxmatched_sort, ignore_index=True)
+        output_df_list.append(df_orig_same_blobpath_matched)
 
-    assert len(df_output) == len(df_imported)
+    sys.stdout.write('\r')
+    sys.stdout.write('Progress: [{:{}}] ({:7d} / {:7d})'.format('|' * progbar_length, progbar_length,  n, n))
 
-    print(f'The number of the total particles: {len(df_output)}')
+    print('\nConcatenating dataframes...')
+    df_out = pd.concat(output_df_list, ignore_index=True)
+
+    assert len(df_out) == len(df_imported)
+
+    print(f'The number of the total particles: {len(df_out)}')
 
 
 if __name__ == '__main__':
